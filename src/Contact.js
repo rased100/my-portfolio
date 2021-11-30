a78ea8386dd4fbd77c8ebb3725befdc7930741a5
rased100 <rased100@gmail.com>
2021-11-30 23:32:32 +0600

    form add

--- src/Contact.js
+++ src/Contact.js
@@ -12,7 +12,11 @@ function Contact() {
                             <p className="hire__text white">I am available for web development work. Connect with me via phone:</p>
                             <p className="hire__text white"><strong>+8801716-738-111</strong> or email <strong>rased100@gmail.com</strong></p>
                         </div>
+
+                        {/* <div class="jotform-table-embed" style="width: 100%; height: 600px;" data-id="213334517106043" data-type="interactive"></div><script>(function(doc, id){let scripts=doc.getElementsByTagName("script")[0]; if (!doc.getElementById(id)){let script=doc.createElement("script"); script.async=1; script.id=id; script.src="https://cdn.jotfor.ms/common/tableEmbed.js"; scripts.parentNode.insertBefore(script, scripts);}})(document, "jotform-async");</script>
+                        <script type="text/javascript" src="https://form.jotform.com/jsform/213334517106043"></script> */}
                         <div className="input__box">
+
                             <input type="text" className="contact name" placeholder="Your name *" />
                             <input type="text" className="contact email" placeholder="Your Email *" />
                             <input type="text" className="contact subject" placeholder="Write a Subject" />
